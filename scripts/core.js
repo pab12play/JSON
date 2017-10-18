e8cbcc9a11eee42f4758a2174fa459fdcaf43293
Pablo Pineda <pablo12.izq@gmail.com>
2017-10-18 16:56:14 -0600

    Added print XML function

--- scripts/core.js
+++ scripts/core.js
@@ -7,15 +7,25 @@ function addValues(){
 	var valor = document.getElementById("valor").value;
 	dictionary[llave] = valor;
 	printJSON();
+	printXML();
 }
 
 function printJSON(){
 	var string = '{<br>'
 	for(var key in dictionary){
-		string += '\t"'+key+'":"'+dictionary[key]+'"<br>';
+		string += '&nbsp;"'+key+'":"'+dictionary[key]+'"<br>';
 	}
 	string += '}'
 	document.getElementById("format").innerHTML=string;
+}
+
+function printXML(){
+	var string = '&lt;ictionary&gt;<br>'
+	for(var key in dictionary){
+		string += '&nbsp;&lt;item '+key+'="'+dictionary[key]+'"&gt;<br>';
+	}
+	string += '&lt;/dictionary&gt;'
+	var display = document.getElementById("format").innerHTML=string;
 }
 
 window.onload = function(){
